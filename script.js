b0fb01add2bd19a733d10b569f7149f42ea63928
earl2art <earl2art@gmail.com>
2016-06-29 19:46:32 -0700

    My finished Assignment 2 on June 29th.

---
+++ script.js
@@ -0,0 +1,191 @@
+//step 1
+/*
+var someMonth();
+function theMonth();
+var currentMonth();
+var summerMonth();
+function myLibrary();
+*/
+
+//step 2
+/*
+4.0         // numeric literal
+"Hello!"    // string literal
+false       //Boolean literal
+null        //literal null value
+*/
+
+//step 3 complex/varible expressions
+/*
+var simpleMath = 4 * 5 + 2;
+var simplerMath = (6/3) + 2;
+*/
+
+//step 4 declare 9 variables
+/*
+var firstName,lastName, yourAddress, yourCity, yourState, yourZipCode, myAge, referSource, contactYou;
+*/
+
+//step 5 assign 3 variables
+/*
+var firstName = "Earl Jackson", yourZipCode = 92105, referSource = true;
+*/
+
+//step 6
+/*
+var weeksLearningJava = 3;
+console.log("Earl has been studing Javascript for " + weeksLearningJava + " weeks.");
+*/
+
+/*
+var myName = "Earl Jackson", isStudent = true;
+console.log("Is " + myName + " a student?" + isStudent);
+*/
+
+/*
+var x = 2 + 2, isCorrect = true;
+console.log("2+2= " + x + isCorrect);
+*/
+
+//step 7
+/*
+var name = "Earl";
+console.log("My name is " + name + " " + lastName + ".");
+var lastName = "Jackson";
+*/
+
+//step 8
+/*
+var someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+console.log(someString);
+*/
+
+//step 9
+/*
+var x = 300;
+x = null;
+console.log(x);
+*/
+
+/*
+var earl;
+console.log(earl);
+*/
+
+//step 10
+/*
+var name = typeof "Earl";
+console.log(name);
+*/
+
+/*var decimalNumber = typeof 2.5;
+console.log(decimalNumber);
+*/
+
+/*
+var carName = typeof false;
+console.log(carName);
+*/
+
+/*
+var dateOf = typeof new Date();
+console.log(dateOf);
+*/
+
+/*
+var someThing = typeof someThing;
+console.log(someThing);
+*/
+
+//step 11
+
+/*
+var name = "Earl Jackson";
+var classType = "FEWD 2";
+alert("Hello " + name + ", welcome to the " + classType + " class!");
+*/
+
+//step 12
+
+/*
+var name = "Earl Jackson";
+var classType = "FEWD 2";
+alert("Hello " + name + ", welcome to the " + classType + " class!");
+*/
+
+//step 13
+/*
+var course = "JavaScript"
+console.log("Earl has been studing " + course + " for weeks.");
+*/
+
+//step 14
+/*
+var name = "Earl Jackson", course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+//step 15
+/*
+prompt("What is your name?");
+*/
+
+//step 16
+/*
+prompt("What class are you taking?");
+*/
+
+//step 17
+/*
+var x = 10, y = 20;
+console.log(x + y);
+*/
+
+//step 18
+/*
+var x = 20;
+console.log(x + 20);
+*/
+
+//step 19
+/*
+var x = 20;
+console.log(x * 5);
+*/
+
+//step 20
+/*
+var x = 2;
+console.log(x / 1);
+*/
+
+//step 21
+/*
+var x = 300, y = 400;
+console.log(x != y);
+*/
+
+//step 22
+/*
+var x = 600, y = 700;
+console.log(x==y);
+*/
+
+//step 23
+/*
+var widget = new Object();
+widget.kind = "Wrench";
+console.log(typeof widget)
+*/
+
+//step 24
+/*
+var widget = new Object();
+console.log(widget instanceof Object);
+*/
+
+//step 25
+/*
+var widget = new Object();
+console.log(widget !== Object);
+*/
